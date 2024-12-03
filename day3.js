8fd290dc01f2be5ffb0171d105e3fead3a29f55b
Exonity <140925674+Exonity1@users.noreply.github.com>
2024-12-03 05:37:46 +0000

    solve day 3 and prepare day 4

--- day3.js
+++ day3.js
@@ -1,6 +1,55 @@
 const fs = require("fs");
-const filePath = "day3_input.txt"; 
+const filePath = "day3_input.txt";
 
-function a(filePath){
+function a(filePath) {
     
+    const content = fs.readFileSync(filePath, "utf8");
+
+    
+    const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+
+    let match;
+    let totalSum = 0;
+
+   
+    while ((match = regex.exec(content)) !== null) {
+        const x = parseInt(match[1], 10);
+        const y = parseInt(match[2], 10);
+
+        totalSum += x * y;
+    }
+
+    console.log(totalSum);
+    return totalSum;
 }
+
+function b(filePath) {
+    
+    const content = fs.readFileSync(filePath, "utf8");
+
+    const regex = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
+
+    let doEnabled = true; 
+    let totalSum = 0;
+    let match;
+    
+    while ((match = regex.exec(content)) !== null) {
+        
+        if (match[0] === "do()") {
+            doEnabled = true; 
+        } else if (match[0] === "don't()") {
+            doEnabled = false; 
+        } else if (match[0].startsWith("mul(") && doEnabled) {
+            
+            const x = parseInt(match[1], 10);
+            const y = parseInt(match[2], 10);
+            totalSum += x * y;
+        }
+    }
+
+    console.log("Total Sum:", totalSum);
+    return totalSum;
+}
+
+b(filePath)
+//a(filePath);
---
+++ day4.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = "day3_input.txt";
+
+function a(filePath) {
+    
+        
+}
+
+
+a(filePath);
